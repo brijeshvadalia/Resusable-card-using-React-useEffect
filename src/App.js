0c9b486d748c41426ce53071f6e536d9f461ac2b
B-8128 <brijeshvadaliya1@gmail.com>
2022-11-30 15:37:17 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Card from './components/Card';
+
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [details, setDetails] = useState({})
+
+  const fetchDetails = async () => {
+    const { data }  = await axios.get("https://randomuser.me/api/")
+    const details = data.results[0]
+    setDetails(details)
+    console.log(details);
+  }
+
+  useEffect(() => {
+    fetchDetails()
+  }, [])
+  
+
+  return (
+    <div className="App">
+
+      <Card details={details} />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+
+
+function Card({details}) {
+
+  return (
+    <div className="card" style={{width: "18rem"}}>
+  <img src={details.picture?.large} className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">Name - {details.name?.first} </h5>
+    <h5 className="card-title">Location - {details.location?.country}</h5>
+    <h5 className="card-title">Postal Code - {details.location?.postcode}</h5>
+    <h5 className="card-title">Gender - {details.gender}</h5>
+    <h5 className="card-title">Age - {details.dob?.age}</h5>
+    <h5 className="card-title">Contact Number - {details.phone}</h5>
+  
+    <a href="#" className="btn btn-primary">Details</a>
+  </div>
+</div>
+  )
+}
+
+export default Card
